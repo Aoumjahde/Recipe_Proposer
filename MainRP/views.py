0f06bec124a0959c0828cc61718084623d4a4f0c
sferratore.francescopaolo00@gmail.com <sferratore.francescopaolo00@gmail.com>
2024-11-22 11:57:03 +0100

    Updated graphics

--- MainRP/views.py
+++ MainRP/views.py
@@ -24,7 +24,7 @@ def index(request):
                 max_tokens=150,
                 temperature=0.7
             )
-            return HttpResponse(response.choices[0].message.content)
+            return render(request, 'MainRP/recipe_display.html', {'recipe_paragraph': response.choices[0].message.content})
     else:
         form = RecipeForm()
 
